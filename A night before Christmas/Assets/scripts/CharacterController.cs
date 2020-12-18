1a2c3f9a4fd4336a1e52996e59bbc95dfcbd1924
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-12-18 10:21:30 +0100

    initial commit + P movement start

---
+++ A night before Christmas/Assets/scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float jumpCount = 2f;
+    float moveSpeed = 200f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Move(float input)
+	{
+        Vector2 motion = rb.velocity;
+        motion.x = input * moveSpeed * Time.deltaTime;
+        rb.velocity = motion;
+    }
+}
---
+++ A night before Christmas/Assets/scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : CharacterController
+{
+    float input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	private void FixedUpdate()
+	{
+        input = Input.GetAxis("Horizontal");
+        Move(input);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
