5f05a8d724edf7a81c4dc6eb7d23016bab3ed13d
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-12-18 11:50:50 +0100

    added presents + Display on screen

---
+++ A night before Christmas/Assets/scripts/DisplayPresentCountToScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayPresentCountToScreen : MonoBehaviour
+{
+    TextMeshProUGUI tmp;
+    Presents present;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tmp = gameObject.GetComponent<TextMeshProUGUI>();
+        present = GameObject.FindGameObjectWithTag("Player").GetComponent<Presents>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp.text = "PRESENTS: " + present.presentCount;
+    }
+}
---
+++ A night before Christmas/Assets/scripts/Presents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presents : MonoBehaviour
+{
+    public int presentCount { get; private set; }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (collision.collider.CompareTag("Present"))
+            presentCount += 1;
+	}
+}
